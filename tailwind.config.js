7e60132a283bd9cae8b7adf2898ab955c189c368
Krrupa <sudonthineni@gmail.com@users.noreply.github.com>
2022-04-20 19:57:00 +0530

    intial set-up

---
+++ tailwind.config.js
@@ -0,0 +1,22 @@
+module.exports = {
+  content: [
+    "./pages/**/*.{js,ts,jsx,tsx}",
+    "./components/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    screens: {
+      sm: '480px',
+      md: '768px',
+      lg: '976px',
+      xl: '1440px',
+    },
+    colors: {
+      'background' :'#fffffc',
+      'darkBrown' :'#333232',
+      'primaryPink': '#F7B2AD' ,
+      'secondaryGreen':'#9ABCA7'
+
+    }
+  },
+  plugins: [],
+}
